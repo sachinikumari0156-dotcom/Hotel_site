e09ab30c52f4c297b6f0bf4ddb1f3672792186e2
sachini kumudu kumari <sachinikumari0156@gmail.com>
2026-02-02 12:58:11 +0530

    improve the java compatible

---
+++ src/main/java/com/zerotrace/smartfacility/SmartFacilityApplication.java
@@ -0,0 +1,18 @@
+package com.zerotrace.smartfacility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableCaching
+@EnableJpaAuditing
+public class SmartFacilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFacilityApplication.class, args);
+    }
+}
